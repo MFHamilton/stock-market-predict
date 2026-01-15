ead1cab0835a6455b692727673759b5a3d73dda7
MFHamilton <mariahamiltonr1@gmail.com>
2026-01-14 22:20:04 -0400

    💡 docs(domain): Add code comments
    Explains the reason as to why this class exists

--- src/domain/entities/market_data.py
+++ src/domain/entities/market_data.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass
 from datetime import date
 
+# Esta clase busca amntener los principios de la arquitectura limpia donde se mantiene una independencia entre
+# los distintos componentes del sistema.
+# Aqu[i se declaran las variables cables del mercado de valores.
+
 class market_data:
     date: date
     open: float
